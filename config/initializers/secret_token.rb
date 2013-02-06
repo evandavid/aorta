9842c73bdfdeef5227c50e8a3271eaca10cc6752
Evan D. Kristian <ev.kristian@gmail.com>
2013-02-07 06:50:39 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aorta::Application.config.secret_token = '8504b23e7d96df42317696a6d367f72eaf96d4e09e7174531cf7f4c0fcf35365bdb5116cf736a1e800db48990b6d0fadf48157ed86eb97ef46308fe719ed6af0'
